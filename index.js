0b3f732420c3f45ff691ef3a5bcbd1ab0883e79c
Dan <bickel09@yahoo.com>
2022-02-02 14:45:15 -0600

    Fin

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let userName = window.prompt('Please enter your name:')
 let userScore = 0
+let playAgain = true
 
+function playGame() {
 for(let i = 0; i < questions.length; i++) {
     let question = questions[i]
     let userAnswer = window.prompt(question.text)
@@ -10,3 +12,14 @@ for(let i = 0; i < questions.length; i++) {
 }
 
 window.alert('Your Score is: '+userScore)
+}
+while (playAgain === true) {
+    playGame()
+    let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+    if(userChoice === 'yes') {
+            playAgain = true
+        }   else {
+            playAgain = false
+            window.alert('Thanks for playing the game!')
+        }
+}
